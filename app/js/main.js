d32166ac9255a419287bda31ae7b4b2a3008102f
LioraProgram7 <sv.kiselis@gmail.com>
2022-05-30 19:40:48 +0300

    corrected all pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
 
+    $('.phrases__checkbox-plus').on('click', function () {
+      $(this.parentElement.parentElement).toggleClass('active');
+      $(this).toggleClass('active');
+    });
+
+
+    // Изменила поведение кнопки
+    $('#securityBtn').on('click', function () {
+      $('.header__security-list').slideToggle();
+    });
+
 });
